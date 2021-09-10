0b4f322b74c6b8db3151ce4b8b5126d5aeddb224
Julia <62606982+themeriff117@users.noreply.github.com>
2021-09-10 19:24:13 -0400

    Finished Styling using CSS and Bootstrap 4

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,12 +1,13 @@
 // Elements
 const welcomeScreen = document.getElementById(`welcome-screen`);
-const gameScreen = ;
-const startGameButton = ;
-const userName = ;
-const userSelection = ;
-const goButton = ;
-const scoreParagraph = ;
-const gameHistoryParagraph = ;
+const gameScreen = document.getElementById('game-screen');
+//  const gameScreen = document.querySelector(`#game-screen`);
+const startGameButton = document.getElementById('start-game-button');
+const userName = document.getElementById('user-name');
+const userSelection = document.getElementById('user-selection');
+const goButton = document.getElementById('go-button');
+const scoreParagraph = document.getElementById('score');
+const gameHistoryParagraph = document.getElementById('game-history');
 
 // instantiate the game object from the `RockPaperScissors` class.
 let game;
@@ -29,9 +30,20 @@ startGameButton.addEventListener(`click`, function () {
   const username = 
   game = new RockPaperScissors(userName);
   // Complete
+  // ^^ Does this get the username on click a?
 });
+
+hideWelcomeScreen.addEventListener('click', function ()) {
+  const hidewelcomescreen = 
+  hidescreen = new RockPaperScissors();
+  //If you hit c, does this hide the welcome screen?
+}
 
 // go-button EventListener
 startGameButton.addEventListener(`click`, function () {
   
 });
+
+function updateScoreTallyUI() {
+  //step 3
+}
--- resources/scripts/rock_paper_scissors.js
+++ resources/scripts/rock_paper_scissors.js
@@ -13,9 +13,9 @@ class RockPaperScissors {
    * using Math.random() method, you should be able to get one of the following values
    */
   generateCPUResponse(){
-    const acceptedValues = [ `rock`, `paper`, `scissors` ];
-    const randomrps = acceptedValues[Math.floor(Math.random() * acceptedValues.length)];
-    //QUESTION: should I be using 'let' instead of 'const'?? I think I'm confusing them.
+    let acceptedValues = [ `rock`, `paper`, `scissors` ];
+    let randomrps = acceptedValues[Math.floor(Math.random() * 3)];
+    // let > var
     return randomrps;
   }
   /**
@@ -67,6 +67,7 @@ class RockPaperScissors {
         return 'tie';
       }
     }
+    return 'lose';
 
    }
 
@@ -75,42 +76,9 @@ class RockPaperScissors {
    * @param {string} userSelection user selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
    */
   play(userSelection){
-    console.WriteLine("Enter your name: ");
-    string userName = Console.ReadLine();
-    console.WriteLine("Let's begin playing, " + userName);
-
-    //make buttons of rps 
-    document.getElementById('generate').onclick = function () {
-
-      let rpsvalues = ["rock", "paper", "scissors"];
-      //QUESTION: What replaces 'var' again? That's the old way, and isn't the new way let or const?
-      let select = document.createElement("select");
-      select.name = "rps";
-      select.id = "rps"
-
-      for (const val of rpsvalues) {
-        var option = document.createElement("option");
-        option.value = val;
-        option.text = val.charAt(0).toUpperCase() + val.slice(1);
-        select.appendChild(option);
-      }
-
-      var label = document.createElement("label");
-      label.innerHTML = "Make your move: "
-      label.htmlFor = "rps";
-
-      document.getElementById("container").appendChild(label).appendChild(select);
-      //end button Selection
-
       //Call what the computer randomly generated
-      let comResult = generateCPUResponse.call(randomrps);
-
-      console.WriteLine("The computer choose: " + comResult + "! Your answer: " + option);
-      //is 'option' what the user selected? I'm a little confused about that
-      
-      //Call the determineWinner method: determineWinner(userSelection, cpuSelection)
-      return determineWinner.call(select, comResult, option);
-
+      let cpuResult = this.generateCPUResponse(); 
+      let myResult = determineWinner(userSelection, cpuResult);
     }
 
 
